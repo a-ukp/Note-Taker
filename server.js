b6d00d6a03d042e58ccadac924527a65d29caba8
a-ukp <uyenkphung@gmail.com>
2021-05-13 22:05:50 -0500

    created/added base files

---
+++ server.js
@@ -0,0 +1,8 @@
+// declare dependencies
+const express = require ('express');
+const fs = require ('fs');
+const path = require ('path');
+
+// initialize app
+const app = express();
+const port = process.env.port || 8000;
