f0f0e8e0be79a8329bfaa0f8c55f3394ae953c72
a-ukp <uyenkphung@gmail.com>
2021-05-15 02:09:30 -0500

    added html routing

--- routes.js
+++ routes.js
@@ -1,3 +1,9 @@
 // api routing
 
 // html routing
+module.exports = (app) => {
+    // get notes html page when /notes at end of url
+    app.get ('/notes', (req,res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+    app.get ('*'), (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'));
+}
--- server.js
+++ server.js
@@ -6,20 +6,15 @@ const path = require ('path');
 // initialize express app
 const app = express();
 
-//testing get
-// app.get('/', (req,res) => {
-//     res.send('hello jupiter');
-// });
-
-//testing app.get
-// app.get('/', (req,res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-// set static folder
+// middleware to access other files
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// communicate with route file for api and html
+require('./routes')(app);
 
 // check for available port first before running on 8000
 const PORT = process.env.port || 8000;
 
+// listen command
 app.listen(PORT, () => console.log(`Listening to server on ${PORT}`));
