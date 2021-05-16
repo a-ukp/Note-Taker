8553e5671b90900fdc3ff44ab633f7554f3fdb7f
a-ukp <uyenkphung@gmail.com>
2021-05-15 20:30:05 -0500

    node in gitignore

--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ module.exports = (app) => {
     app.get ('/notes', (req,res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
     app.get ('*'), (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'));
+    app.get ('/'), (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'));
 
     // api routing
     // reads json file at /api/notes
